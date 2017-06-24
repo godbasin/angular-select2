073fdd19945c7443dd5477136ce4c8262f6bad06
wangbeishan <wangbeishan@sensetime.com>
2017-06-24 12:12:05 +0800

    updated

--- src/select2.component.ts
+++ src/select2.component.ts
@@ -2,7 +2,7 @@
  * desc：select2
  * how to use: <select2 [(ngModel)]='your_prop' [options]='your_options' [disabled]='your_condition' ></select2>
  */
-import $ from 'jquery';
+const $ = require('jquery');
 require('./select2/select2.min.js');
 import { Component, Input, Output, AfterViewInit, ElementRef, EventEmitter, OnChanges } from '@angular/core';
 import { CustomInputComponent, customInputAccessor } from './custom-input';
@@ -16,17 +16,17 @@ import { CustomInputComponent, customInputAccessor } from './custom-input';
 // ControlValueAccessor: A bridge between a control and a native element.
 export class Select2Component extends CustomInputComponent implements OnChanges, AfterViewInit {
     @Input() options: any[] = []; // object: {id, text} or array: []
-    @Input() params: object = {};
     @Input() disabled: boolean = false;
     @Output() select = new EventEmitter<any>();
 
     select2: any;
-    private el;
+    private el: ElementRef;
 
     constructor(el: ElementRef) {
         super();
         this.el = el;
     }
+
     ngAfterViewInit() {
         this.select2.select2('val', [this.value]);
     }
@@ -34,7 +34,7 @@ export class Select2Component extends CustomInputComponent implements OnChanges,
     ngOnChanges() {
         this.select2 = $(this.el.nativeElement).find('select').select2({
             data: this.options,
-        }).on("select2:select", (ev) => {
+        }).on("select2:select", (ev: any) => {
             const { id, text } = ev['params']['data'];
             this.value = id;
             this.select.emit({ id, text });
