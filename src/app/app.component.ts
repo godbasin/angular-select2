e2a872826ef45e286719c1ac0308ca54d4b63678
wangbeishan <wangbeishan@sensetime.com>
2017-12-04 17:02:11 +0800

    fix when options change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 
+const options1: any[] = ['1-1', '1-2', '1-3', '1-4', '1-5'];
+const options2: any[] = ['2-1', '2-2', '2-3', '2-4', '2-5'];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.min.css',]
 })
 export class AppComponent {
-  options: any[] = ['选项1', '选项2', '选项3', '选项4', '选项5'];
+  options: any[] = options1;
   optionSelected: any;
+  inputVal: string = '';
 
   onSelect({id, text}){
     alert(`id: ${id}, text: ${text}`);
+    console.log(this.optionSelected)
+  }
+
+  changeOptions(){
+    this.options = this.options == options1 ? options2 : options1;
+    console.log(this.options)
+  }
+
+  changeValue(){
+    this.optionSelected = this.inputVal;
   }
 }
