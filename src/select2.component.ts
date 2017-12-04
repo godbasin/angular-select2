daf79d71dd98874764cff9ad1d3e1ede5f43e57e
wangbeishan <wangbeishan@sensetime.com>
2017-12-04 16:56:34 +0800

    fix when options change

--- src/select2.component.ts
+++ src/select2.component.ts
@@ -5,7 +5,8 @@
 declare var require: any
 const $ = require('jquery');
 require('./select2/select2.min.js');
-import { Component, Input, Output, AfterViewInit, ElementRef, EventEmitter, OnChanges, ViewEncapsulation } from '@angular/core';
+import { Component, Input, Output, AfterViewInit, ElementRef, EventEmitter, OnChanges, OnDestroy, 
+    ViewEncapsulation, SimpleChanges } from '@angular/core';
 import { CustomInputComponent, customInputAccessor } from './custom-input';
 
 @Component({
@@ -16,7 +17,7 @@ import { CustomInputComponent, customInputAccessor } from './custom-input';
     providers: [customInputAccessor(Select2Component)]
 })
 // ControlValueAccessor: A bridge between a control and a native element.
-export class Select2Component extends CustomInputComponent implements OnChanges, AfterViewInit {
+export class Select2Component extends CustomInputComponent implements OnChanges, OnDestroy, AfterViewInit {
     @Input() options: any[] = []; // object: {id, text} or array: []
     @Input() disabled: boolean = false;
     @Output() onSelect = new EventEmitter<any>();
@@ -33,13 +34,31 @@ export class Select2Component extends CustomInputComponent implements OnChanges,
         this.select2.select2('val', [this.value]);
     }
 
-    ngOnChanges() {
-        this.select2 = $(this.el.nativeElement).find('select').select2({
-            data: this.options,
-        }).on("select2:select", (ev: any) => {
-            const { id, text } = ev['params']['data'];
-            this.value = id;
-            this.onSelect.emit({ id, text });
-        });
+    ngOnChanges(changes: SimpleChanges) {
+        // Checking if the plugin is initialized
+        if (this.select2 && this.select2.hasClass("select2-hidden-accessible")) {
+            // Select2 has been initialized
+            if(changes.options){
+                // options change
+                this.select2.empty();
+                this.select2.select2({data: this.options});
+            }else if(changes.value){
+                // value change
+                this.select2.select2('val', [this.value]);
+            }
+            this.select2.trigger('change');
+        }else{
+            this.select2 = $(this.el.nativeElement).find('select').select2({
+                data: this.options,
+            }).on('select2:select', (ev: any) => {
+                const { id, text } = ev['params']['data'];
+                this.value = id;
+                this.onSelect.emit({ id, text });
+            });
+        }
+    }
+
+    ngOnDestroy(){
+        this.select2.select2('destroy');
     }
 }
