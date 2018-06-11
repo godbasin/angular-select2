750237c3420e4ea59d9af9b83d5a83f33436e5e6
godbasin <wangbeishan@163.com>
2018-06-11 21:20:12 +0800

    settings update, fix #30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,35 @@ const options2: any[] = ['2-1', '2-2', '2-3', '2-4', '2-5'];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.min.css',]
+  styleUrls: [
+    './app.component.css',
+    '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+  ]
 })
 export class AppComponent {
   options: any[] = options1;
   optionSelected: any;
   optionsSelected: any;
+  settingsOptionSelected: any;
   inputVal: string = '';
+  settings: any = { placeholder: 'Changing the settings', data: [{id: 1, text: '1'}, {id: 2, text: '2'}], width: '50%' };
 
-  onSelect({id, text}){
+  onSelect({ id, text }) {
     alert(`id: ${id}, text: ${text}`);
-    console.log(this.optionSelected)
+    console.log(this.optionSelected);
   }
 
-  changeOptions(){
+  changeOptions() {
     this.options = this.options == options1 ? options2 : options1;
-    console.log(this.options)
+    console.log(this.options);
   }
 
-  changeValue(){
+  changeValue() {
     this.optionSelected = this.inputVal;
     this.optionsSelected = [this.inputVal];
+  }
+
+  changeSettings() {
+    this.settings = { placeholder: 'Changing the settings', data: [{id: 3, text: '3'}, {id: 4, text: '4'}], width: '50%' };
   }
 }
